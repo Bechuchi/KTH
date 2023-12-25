87e83fbaa8447af73907bdee2eacf30fe2d255e9
Bechuchi <denbuw@gmail.com>
2023-12-25 18:14:01 +0100

    ESP8266 module with AT command and OK response

--- BeehiveMonitoringSystem/Client/client.ino
+++ BeehiveMonitoringSystem/Client/client.ino
@@ -1,45 +1,22 @@
-#include "HX711.h"
-#include <EEPROM.h>
+#include <SoftwareSerial.h>
 
-// HX711 circuit wiring
-const int LOADCELL_DOUT_PIN = 2;
-const int LOADCELL_SCK_PIN = 3;
+SoftwareSerial esp8266(10, 11); // RX, TX
 
-HX711 scale;
-
-/**
-Sensor
-*/
-void sensorSetup() {
-  Serial.begin(38400);
-  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
-  calibration();
-}
-
-void calibration() {
-  scale.set_scale(2280.f);  // kalibreringsvärde
-  scale.tare();             // nollställer skalan
-}
-
-void updateMemory(int address, int value) {
-  byte lowByte = value & 0xFF; // Isolerar de lägre 8 bitarna
-  byte highByte = (value >> 8) & 0xFF; // Skiftar och isolerar de högre 8 bitarna
-
-  EEPROM.update(address, lowByte); // Skriver den lägre byten
-  EEPROM.update(address + 1, highByte); // Skriver den högre byten
-}
 
 void setup() {
-  sensorSetup();
-  int currentWeight = scale.get_units(10); // Beräknar medelvärdet av 10 mätningar för exakthet
-  Serial.print("Dagens vikt: ");
-  Serial.println(currentWeight, 1); // Skriver ut dagens vikt
+  Serial.begin(115200);       // Starta seriell kommunikation med Serial Monitor
+  esp8266.begin(115200);      // Starta kommunikation med ESP8266
 
-  // Anta att vi börjar skriva från adress 0
-  updateMemory(0, currentWeight);
+  Serial.println("Skickar AT till ESP8266");
+  esp8266.println("AT");      // Skicka AT-kommandot till ESP8266
 }
 
 void loop() {
-  boolean signalAvailable = true;
-  
+  if (esp8266.available()) {  // Kontrollera om ESP8266 har skickat ett svar
+    Serial.write(esp8266.read()); // Skriv ut vad ESP8266 skickar till Serial Monitor
+  }
+
+  if (Serial.available()) {   // Kontrollera om det finns data skickat till Serial Monitor
+    esp8266.write(Serial.read()); // Skicka det till ESP8266
+  }
 }
